7724e59aba055fb4d799b1c2ca4573a046fc3667
Erich Sauer <erich.sauer@gmail.com>
2021-11-29 12:06:40 -0800

    slight update

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 export const Display = ({head, middle, bottom, shoes, catchphrases, handleCatchphraseRemove}) => {
   return (
     <article>
-      <div >{head}</div>
-      <div >{middle}</div>
-      <div >{bottom}</div>
-      <div >{shoes}</div>
+      <div style={{fontSize: '3rem', height: '3rem'}}>{head}</div>
+      <div style={{fontSize: '3rem', height: '3rem'}}>{middle}</div>
+      <div style={{fontSize: '3rem', height: '3rem'}}>{bottom}</div>
+      <div style={{fontSize: '3rem', height: '3rem'}}>{shoes}</div>
       {catchphrases.map((catchphrase, i) => <div onClick={() => handleCatchphraseRemove(catchphrase)} key={catchphrase + i}>{catchphrase}</div>)}
     </article>
   )
